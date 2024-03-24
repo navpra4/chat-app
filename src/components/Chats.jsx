4a308fca349de536c20a9fc71d66a7d6e97ae7a8
navpra4 <naveenpra4@gmail.com>
2024-03-24 12:15:25 +0530

    Finished the app

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,12 +2,13 @@ import React, { useContext, useEffect, useState } from 'react'
 import {AuthContext} from "../context/AuthContext"
 import { doc, onSnapshot } from 'firebase/firestore'
 import { db } from '../firebase'
-
+import {ChatContext} from "../context/ChatContext"
 
 const Chats = () => {
 
   const [chats,setChats] = useState([])
   const {currentUser} = useContext(AuthContext)
+  const {dispatch} = useContext(ChatContext)
   useEffect(()=>{
     const getChats = ()=>{
       const unsub = onSnapshot(doc(db,"userChats",currentUser.uid),(doc)=>{
@@ -21,10 +22,13 @@ const Chats = () => {
     currentUser.uid && getChats()
   },[currentUser.uid])
 
+  const handleSelect = (u)=>{
+    dispatch({type:"CHANGE_USER",payload:u})
+  }
   return (
     <div className='chats'>
-      {Object.entries(chats)?.map((chat)=>(
-        <div className='userChat' key={chat[0]}>
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat)=>(
+        <div className='userChat' key={chat[0]} onClick={()=>handleSelect(chat[1].userInfo)}>
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className='userChatInfo'>
             <span>{chat[1].userInfo.displayName}</span>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
 
-const Message = () => {
+const Message = ({message}) => {
+  const {currentUser} = useContext(AuthContext)
+  const {data} = useContext(ChatContext)
+
+  const formatTime = (timestamp) => {
+    const date = new Date(timestamp.toDate());
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  };
+  const ref = useRef()
+
+  useEffect(()=>{
+    ref.current?.scrollIntoView({behaviour:"smooth"})
+  },[message])
   return (
-    <div className='message owner'>
+    <div ref = {ref} className={`message ${message.senderId === currentUser.uid && 'owner'}`}>
       <div className="messageInfo">
-        <img src="https://cdn.pixabay.com/photo/2017/03/27/15/11/action-2179304_1280.jpg" alt="" />
-        <span>just now</span>
+        <img src={
+          message.senderId === currentUser.uid?
+          currentUser.photoURL:data.user.photoURL
+        } alt="" />
+        <span>{formatTime(message.date)}</span>
       </div>
       <div className="messageContent">
-        <p>hello</p>
-        <img src="https://cdn.pixabay.com/photo/2017/03/27/15/11/action-2179304_1280.jpg" alt="" />
-      </div>
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div> 
 
     </div>
   )
